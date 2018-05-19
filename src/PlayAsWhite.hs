0b68e55bc2f8ce9aa5a467e2742993668efa402a
u6611178 <u6611178@anu.edu.au>
2018-05-19 17:32:31 +1000

    Finish warm up

--- src/PlayAsWhite.hs
+++ src/PlayAsWhite.hs
@@ -1,15 +1,29 @@
 module PlayAsWhite where
 
 import State
+import Move
 
--- import Move
 -- import Data.List
 -- import Debug.Trace (trace)
 makeMove :: State -> Lookahead -> Moves
-makeMove _ l
+makeMove s l
   | primesUnder (l * l) < 0 = []
   | otherwise =
-    concat $ replicate 4 $ reverse [(x, y) | x <- [0 .. 24], y <- [1 .. 6]]
+      legalMoveBot s
+
+-- get ideas from tutor and friend
+legalMoveBot::State -> Moves
+legalMoveBot s@(State _ _ _ ml _ _ _ _) =
+  case ml of
+   x:xs -> case legalMoves s of
+      y:ys -> head (legalMoves s):legalMoveBot s'
+      [] -> []
+   [] ->[]
+
+   where
+   s'::State
+   s' = performSingleMove s (head (legalMoves s))
+
 
 primesUnder :: Int -> Int
 primesUnder n = length $ filterPrime [2 .. n]
